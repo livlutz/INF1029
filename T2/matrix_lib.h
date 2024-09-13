e062d2adb1ce5421275de00f4049844a5be263f8
Livia Lutz <livialutz385@gmail.com>
2024-09-12 21:14:01 -0300

    Folder do T2 + add matrix_lib.h

---
+++ T2/matrix_lib.h
@@ -0,0 +1,20 @@
+/*Lívia Lutz dos Santos - 2211055
+Ana Luiza Pinto Marques - 2211960*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Onde:
+height = número de linhas da matriz (múltiplo de 8)
+width = número de colunas da matriz (múltiplo de 8)
+rows = sequência de linhas da matriz (height*width elementos)*/
+
+struct matrix {
+    unsigned long int height;
+    unsigned long int width;
+    float *rows;
+};
+
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
+
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC);
