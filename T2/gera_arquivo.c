cc7c940aa7cc89382532261a8fe069665b9635b6
Conta inf1029-14 <inf1029-14@iac.inf.puc-rio.br>
2024-09-17 13:34:16 -0300

    Testes de 1024 + instruções para rodar o arquivo no servidor da PUC

--- T2/gera_arquivo.c
+++ T2/gera_arquivo.c
@@ -10,21 +10,21 @@ int main(void){
     matrixA = fopen("floats_256_2.0f.dat", "wb");
     matrixB = fopen("floats_256_5.0f.dat", "wb");
 
-    rowsA = (float*)malloc((2048 * 2048)*sizeof(float));
-    rowsB = (float*)malloc((2048 * 2048)*sizeof(float));
+    rowsA = (float*)malloc((1024 * 1024)*sizeof(float));
+    rowsB = (float*)malloc((1024 * 1024)*sizeof(float));
 
     if(rowsA == NULL || rowsB == NULL){
         printf("Erro ao alocar memoria\n");
         return 1;
     }
 
-    for(int i = 0; i < (2048 * 2048);i++){
+    for(int i = 0; i < (1024 * 1024);i++){
         rowsA[i] = 2.0;
         rowsB[i] = 5.0;
     }
 
-    fwrite(rowsA, sizeof(float), (2048 * 2048), matrixA);
-    fwrite(rowsB, sizeof(float), (2048 * 2048), matrixB);
+    fwrite(rowsA, sizeof(float), (1024 * 1024), matrixA);
+    fwrite(rowsB, sizeof(float), (1024 * 1024), matrixB);
     
     fclose(matrixA);
     fclose(matrixB);
@@ -36,9 +36,14 @@ int main(void){
 }
 
 /*Instrucoes pra rodar o trabalho : 
-gcc –std=c11 –mfma -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c
+No servidor da PUC :
+gcc -std=c11 -mfma -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c
+
+Na minha humilde residencia :
+gcc –mfma -mavx2 -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c 
+
 Exemplo de linha de comando:
-matrix_lib_test 5.0 2048 2048 2048 2048 floats_256_2.0f.dat floats_256_5.0f.dat result1.dat result2.dat
+./matrix_lib_test 5.0 1024 1024 1024 1024 floats_256_2.0f.dat floats_256_5.0f.dat result1.dat result2.dat
 Onde,
 5.0 é o valor escalar que multiplicará a primeira matriz;
 8 é o número de linhas da primeira matriz;
@@ -48,4 +53,8 @@ Onde,
 floats_256_2.0f.dat é o nome do arquivo de floats que será usado para carregar a primeira matriz;
 floats_256_5.0f.dat é o nome do arquivo de floats que será usado para carregar a segunda matriz;
 result1.dat é o nome do arquivo de floats onde o primeiro resultado será armazenado;
-result2.dat é o nome do arquivo de floats onde o segundo resultado será armazenado.*/
+result2.dat é o nome do arquivo de floats onde o segundo resultado será armazenado.
+
+Como rodar o gera arquivo no servidor da PUC :
+gcc -std=c11 -Wall -o gera_arquivo gera_arquivo.c
+./gera_arquivo*/
