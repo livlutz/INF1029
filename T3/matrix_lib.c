94c1571b449d28c44121b598c5b38e29e83e7c89
Conta inf1029-14 <inf1029-14@iac.inf.puc-rio.br>
2024-10-01 13:14:50 -0300

    Pasta do T3 com alguns arquivos

---
+++ T3/matrix_lib.c
@@ -0,0 +1,89 @@
+/*Lívia Lutz dos Santos - 2211055
+Ana Luiza Pinto Marques - 2211960*/
+
+#include "matrix_lib.h"
+#include "timer.h"
+
+/*Essa função recebe um valor escalar e uma matriz como argumentos de entrada e calcula o
+produto do valor escalar pela matriz. O resultado da operação deve ser retornado na matriz
+de entrada. Em caso de sucesso, a função deve retornar o valor 1. Em caso de erro, a
+função deve retornar 0*/
+
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
+    
+    //se o ponteiro da matrix for nulo (matrix nao existe), retorna erro
+    if (matrix == NULL){
+        return 0;
+    }
+    __m256 scalar = _mm256_set1_ps(scalar_value);
+
+   // Usar instruções AVX para multiplicar todos os elementos da matriz por um escalar
+   for(int i = 0; i< matrix->height * matrix->width; i += 8){
+        __m256 row = _mm256_load_ps(&matrix->rows[i]);
+        __m256 result = _mm256_mul_ps(row,scalar);
+        _mm256_store_ps(&matrix->rows[i],result);
+   }
+       
+    return 1;
+}
+
+/*Essa função recebe 3 matrizes como argumentos de entrada e calcula o valor do produto da
+matriz A pela matriz B. O resultado da operação deve ser retornado na matriz C. Em caso
+de sucesso, a função deve retornar o valor 1. Em caso de erro, a função deve retornar 0.*/
+
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC){
+
+    /*se o ponteiro de alguma matrix for nulo (matrix nao existe), retorna erro
+    o numero de colunas da primeira matrix tem q ser igual ao numero de linhas da segunda matrix
+    a matriz resultante tem que ter o numero de linhas da primeira matriz e o numero de colunas da segunda matriz*/
+    
+    if((matrixA == NULL) || (matrixB == NULL) || (matrixC == NULL) || (matrixA->width != matrixB->height) || (matrixC->height != matrixA->height) || (matrixC->width != matrixB->width) ){
+        printf("Erro de dimensao ou alocacao\n");
+        return 0;
+    }
+
+   int indexA, indexB, indexC;
+
+    for(int i = 0; i < matrixC->height; i++){
+
+        //itera por colunas da matriz A
+        for(int j = 0; j < matrixA->width;j++){
+            //Calcula posicao inicial do indice da matrizA 
+            indexA = i * matrixA->width + j;
+
+            //valor do elemento da matriz A
+            __m256 valA = _mm256_set1_ps(matrixA->rows[indexA]);
+
+            //itera por colunas da matriz B
+            for (int k = 0; k < matrixB->width; k += 8){
+
+                //Calcula posicao inicial do indice da matrizB
+                indexB = j * matrixB->width + k;
+                //Calcula posicao inicial dos indices da matrizC aqui e depois incrementa o valor dentro do loop
+                indexC = i * matrixC->width + k;
+
+                // Carrega o bloco de 8 elementos da linha j de B
+                __m256 rowB = _mm256_load_ps(&matrixB->rows[indexB]);
+
+                // Carrega o bloco de 8 elementos da linha i de C 
+                __m256 rowC = _mm256_load_ps(&matrixC->rows[indexC]);
+
+                //multiplica cada elemento da linha de A pelo elemento da coluna de B
+                __m256 result = _mm256_fmadd_ps(rowB,valA,rowC);
+
+                //Armazena o resultado na linha i de C
+                _mm256_store_ps(&matrixC->rows[indexC],result);
+            }
+        }
+    }
+
+    return 1;
+}
+
+
+void set_number_threads(int num_threads){
+
+
+
+    return;
+}
---
+++ T3/matrix_lib.h
@@ -0,0 +1,23 @@
+/*Lívia Lutz dos Santos - 2211055
+Ana Luiza Pinto Marques - 2211960*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <immintrin.h>
+
+/*Onde:
+height = número de linhas da matriz (múltiplo de 8)
+width = número de colunas da matriz (múltiplo de 8)
+rows = sequência de linhas da matriz (height*width elementos)*/
+
+struct matrix {
+    unsigned long int height;
+    unsigned long int width;
+    float *rows;
+};
+
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
+
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC);
+
+void set_number_threads(int num_threads);
