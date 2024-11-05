36394625d5742a4e29c6bb97cd1d781b46b7d6f0
Conta inf1029-14 <inf1029-14@iac.inf.puc-rio.br>
2024-11-05 13:39:36 -0300

    Escrevendo algumas instruções

--- T4/gera_arquivo.c
+++ T4/gera_arquivo.c
@@ -41,8 +41,14 @@ int main(void){
 gcc -Wall -o gera_arquivo gera_arquivo.c
 ./gera_arquivo
 
-No servidor da PUC : gcc -std=c11 -pthread -mfma -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c 
+No servidor da PUC : nvcc -o matrix_lib_test matrix_lib_test.cu matrix_lib.cu timer.c
  
-Na minha humilde residencia : gcc -pthread -mfma -mavx2 -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c
+Na minha humilde residencia : n sei ainda
 
-./matrix_lib_test 5.0 2048 2048 2048 2048 4 floats_256_2.0f.dat floats_256_5.0f.dat result1.dat result2.dat */
+matrix_lib_test 5.0 8 16 16 8 256 4096 1024 floats_256_2.0f.dat floats_256_5.0f.dat result1.dat
+result2.dat 
+
+
+256 é o número de threads por bloco a serem disparadas;
+4096 é o número máximo de blocos por GRID a serem usados;
+1024 é a quantidade máxima de memória em MiB que pode ser alocado na GPGPU*/
