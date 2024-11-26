61eb926d4f82b81a6ea9beec34d97550d8c85ef8
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2024-11-26 08:53:33 -0300

    Escrevendo o resto do código (mas falta implementar coisas)

--- sketch_shield_multifuncao_sorteador_numeros.ino
+++ sketch_shield_multifuncao_sorteador_numeros.ino
@@ -1,3 +1,5 @@
+//TEM Q BOTAR OS NOMES
+
 #include // Bibliotec TimerOne
 #include #define RANGE_MAX_NUMBER 9999
 #define RANGE_MIN_NUMBER 0000
@@ -54,17 +56,16 @@ void loop() {
 
   switch(GeneratorModeValue){
     case GENERATOR_STOPPED:
-      GeneratorModeValues = GENERATOR_STOPPED;
       ledModeValue = LED_ALL_OFF;
       MFS.writeLeds(ledModeValue, OFF);
 
       switch(btn){
         case BUTTON_1_SHORT_RELEASE:
           if(RangeStatusValue == RANGE_SET){
-          GeneratorModeValue = GENERATOR_STARTED;
-          AnimationModeValue = ANIMATION_STARTED;
+            GeneratorModeValue = GENERATOR_STARTED;
+            AnimationModeValue = ANIMATION_STARTED;
           }
-        break;
+          break;
 
         case BUTTON_1_LONG_PRESSED:
           //nao faz nada n sei oq botar aqui
@@ -85,18 +86,94 @@ void loop() {
         case BUTTON_3_LONG_PRESSED:
           GeneratorModeValue = SETTING_RANGE_MIN_NUM_STARTED;
           break;
-          
       }
-
       break;
 
       case GENERATOR_STARTED:
+        // deve tocar um beep curto de 50 ms no buzzer e iniciar a animação dos 4 displays de 7 segmentos e a animação sequencial dos 4 leds.
+        MFS.beep(5); 
+        
         break;
 
       case SETTING_RANGE_MAX_NUM_STARTED:
-        break;
+        /*Neste estado, o sorteador deve apresentar o valor máximo da faixa de números aleatórios
+atualmente configurado piscando no display de 7 segmentos*/
+        MFS.writeLeds(LED_ALL_ON, RANGE_MAX_NUMBER);
+        
+         switch(btn){
+          case BUTTON_1_SHORT_RELEASE:
+            /*encerrar o estado de configuração do valor máximo da faixa
+de números aleatórios, mudar a situação de configuração desse valor para
+RANGE_SET, acender o LED 1 e voltar para o estado GENERATOR_STOPPED.*/
+            RangeStatusValue = RANGE_SET;
+            GeneratorModeValue = GENERATOR_STOPPED;
+            ledModeValue = LED_1_ON;
+            
+            break;
+  
+          case BUTTON_1_LONG_PRESSED:
+            //ignorar n sei como faz isso
+            break;
+  
+          case BUTTON_2_SHORT_RELEASE:
+            /*incrementar o valor máximo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos */
+            
+            break;
+  
+          case BUTTON_2_LONG_PRESSED:
+            /*incrementar o valor máximo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos */
+            break;
+          
+          case BUTTON_3_SHORT_RELEASE:
+            /*decrementar o valor máximo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos*/
+            break;
+          
+          case BUTTON_3_LONG_PRESSED:
+            /*decrementar o valor máximo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos*/
+            break;
+        }
+        
+      break;
     
       case SETTING_RANGE_MIN_NUM_STARTED:
+        MFS.writeLeds(LED_ALL_ON, RANGE_MIN_NUMBER);
+
+        switch(btn){
+          case BUTTON_1_SHORT_RELEASE:
+            RangeStatusValue = RANGE_SET;
+            GeneratorModeValue = GENERATOR_STOPPED;
+            ledModeValue = LED_2_ON;
+            break;
+  
+          case BUTTON_1_LONG_PRESSED:
+            //ignorar n sei como faz isso
+            break;
+  
+          case BUTTON_2_SHORT_RELEASE:
+            /*ncrementar o valor mínimo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos */
+            break;
+  
+          case BUTTON_2_LONG_PRESSED:
+            /*ncrementar o valor mínimo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos */
+            break;
+          
+          case BUTTON_3_SHORT_RELEASE:
+            /*decrementar o valor mínimo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos*/
+            break;
+          
+          case BUTTON_3_LONG_PRESSED:
+           /*decrementar o valor mínimo da faixa de números aleatórios
+de 100 unidade e apresentar esse valor piscando no display de 7 segmentos*/
+            break;
+        }
+        
         break;
   }
       
